d99dc7e73b357d087c712d91114646765309ee28
ktr0328 <zf7heatpa@gmail.com>
2018-02-12 16:03:50 +0900

    feat: add year, month, day

--- src/renderer/components/Watch.vue
+++ src/renderer/components/Watch.vue
@@ -6,9 +6,12 @@
       table
         tr
           th(v-for="v in clock.definition") {{v}}
-        clock-table-row(title="h" v-bind:data="clock.hh")
-        clock-table-row(title="m" v-bind:data="clock.mm")
-        clock-table-row(title="s" v-bind:data="clock.ss")
+        clock-table-row(title="y" v-bind:data="clock.year")
+        clock-table-row(title="m" v-bind:data="clock.month")
+        clock-table-row(title="d" v-bind:data="clock.day")
+        clock-table-row(title="h" v-bind:data="clock.hour")
+        clock-table-row(title="m" v-bind:data="clock.minute")
+        clock-table-row(title="s" v-bind:data="clock.second")
 </template>
 
 <script>
@@ -22,9 +25,12 @@
         now: moment(),
         clock: {
           definition: ['', 32, 16, 8, 4, 2, 1],
-          hh: '',
-          mm: '',
-          ss: ''
+          year: '',
+          month: '',
+          day: '',
+          hour: '',
+          minute: '',
+          second: ''
         }
       }
     },
@@ -37,9 +43,12 @@
         this.now = moment()
         const time = this.now
 
-        this.clock.hh = this.to_b(time.format('HH'))
-        this.clock.mm = this.to_b(time.format('mm'))
-        this.clock.ss = this.to_b(time.format('ss'))
+        this.clock.year = this.to_b(time.format('YY'))
+        this.clock.month = this.to_b(time.format('MM'))
+        this.clock.day = this.to_b(time.format('DD'))
+        this.clock.hour = this.to_b(time.format('HH'))
+        this.clock.minute = this.to_b(time.format('mm'))
+        this.clock.second = this.to_b(time.format('ss'))
       },
       to_b: function (time) {
         const length = this.clock.definition.length - 1
